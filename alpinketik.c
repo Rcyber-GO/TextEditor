cb525f6d3f2afd504a71d961c1373fa2a8271b65
apin <alvin.dwi.tif25@polban.ac.id>
2026-06-04 22:32:17 +0700

    Membuat logika untuk menambahkan baris baru yang nantinya ketika sudah mencapai maks kolom akan secara otomatis membuat baris baru dibawahnya dan menambahkan special key enter

--- alpinketik.c
+++ alpinketik.c
@@ -23,6 +23,34 @@ void inisialisasiEditor(TextEditor *ed) {
     ed->jumlah_baris = 1;
 }
 
+void tambahBarisBaru(TextEditor *ed) {
+    // Disini saya mengalokasikan memori untuk membuat sebuah node baru
+    Node *node_baru = (Node*)malloc(sizeof(Node));
+    memset(node_baru->info, 0, MAX_KOLOM);
+    
+    
+    // Proses menyambungkan node yg baru dibuat ke node sebelumnya (prev) dan ke node yg ada di depannya(next). 
+    // Logika ini digunakan untuk proses insert baris ditengah tengah baris supaya baris yg baru bisa nyambung ke baris sebelum dan setelahnya 
+    // Logikanya prev dari node baru ini akan diisi alamat node dari baris sekarang atau node yang sedang aktif(sedang ditempati cursor)
+    // Dan untuk next nya dari node baru akan diisi alamat dari node next dari baris yang sedang aktif
+    node_baru->prev = ed->baris_sekarang; 
+    node_baru->next = ed->baris_sekarang->next; 
+
+    // Kalo logika ini ketika next nya dari baris sekarang tidak NULL(ada node lagi) untuk nyambungnya jadi nanti alamat prev nya dari baris next (setelah) baris sekarang akan diisi si alamat node baru itu
+    // Kalo NULL (kosong/cursor berada di ujung(tail)) alamat dari tail baris sekarang akan diisi alamat node baru
+    if (ed->baris_sekarang->next != NULL) {
+        ed->baris_sekarang->next->prev = node_baru; 
+    } else {
+        ed->tail = node_baru; 
+    }
+    
+    // Logika ini murni untuk tampilan khusunya untuk cursor jadi ketika sudah buat node baru cursor yang tadinya ada di baris sekarang akan di pindahkan ke node baru di indeks ke 0 ujung kiri
+    // Dan yang paling bawah untuk memberitahu program bahwa jumlah barisnya itu nambah 1
+    ed->baris_sekarang = node_baru;
+    ed->kolom_sekarang = 0;
+    ed->jumlah_baris++;
+}
+
 void jalankanEditor(TextEditor *ed) {
     system("cls");
     
@@ -50,6 +78,12 @@ void jalankanEditor(TextEditor *ed) {
             }
         }
         else if (ch >= 32 && ch <= 126) { // Ngetik biasa
+            if (ed->kolom_sekarang >= MAX_KOLOM - 1) {
+                tambahBarisBaru(ed); 
+                posisi_y_layar++;    
+                
+                gotoxy(ed->kolom_sekarang, posisi_y_layar);
+            }
             if (ed->kolom_sekarang < MAX_KOLOM - 1) {
                 ed->baris_sekarang->info[ed->kolom_sekarang] = ch;
                 printf("%c", ch); 
@@ -57,6 +91,15 @@ void jalankanEditor(TextEditor *ed) {
                 ed->kolom_sekarang++;
                 ed->baris_sekarang->jumlah_karakter++;
             }
-        } 
+        }
+        else if (ch == 13){ // ENTER
+			if (ed->jumlah_baris < MAX_BARIS){
+				tambahBarisBaru(ed);
+				posisi_y_layar++;
+			
+			}else{
+			
+			}
+		} 
     }
 }
